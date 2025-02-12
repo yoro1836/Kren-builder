91f9b9b70fffba210fa2c418dbb20cf6ab31ae4d
yoro1836 <95689076+yoro1836@users.noreply.github.com>
2025-02-13 01:55:17 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -132,8 +132,7 @@ if [[ $USE_KSU_NEXT == "yes" ]]; then
 elif [[ $USE_KSU == "yes" ]] && [[ $USE_KSU_SUSFS == "yes" ]]; then
     curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
     git clone https://github.com/rsuntk/KernelSU -b susfs-v1.5.5 $WORKDIR/ksu_tmp
-    cp -rf $WORKDIR/ksu_tmp .
-    rm -rf $WORKDIR/ksu_tmp
+    cp -rf $WORKDIR/ksu_tmp KernelSU
     cd $WORKDIR/KernelSU
     KSU_VERSION=$(git describe --abbrev=0 --tags)
 elif [[ $USE_KSU == "yes" ]]; then
