9b2de07eb957f5697273e0a4b55fc87704b96bb7
yoro1836 <95689076+yoro1836@users.noreply.github.com>
2025-02-16 03:18:46 +0900

    Update build_modules.sh

--- build_modules.sh
+++ build_modules.sh
@@ -75,12 +75,8 @@ EOF
 )"
 
 # Build Modules (수정됨)
-if [[ $BUILD_KERNEL == "yes" ]]; then
-  set +e; (
-    make ARCH=arm64 LLVM=1 LLVM_IAS=1 O=$WORKDIR/out CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- kren_defconfig # -C common 추가 (수정됨)
-    make -j$(nproc --all) ARCH=arm64 LLVM=1 LLVM_IAS=1 O=$WORKDIR/out CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- modules # -C common 유지
-  ) 2>&1 | tee $WORKDIR/build.log; set -e
-fi
+make ARCH=arm64 LLVM=1 LLVM_IAS=1 O=$WORKDIR/out CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- kren_defconfig # -C common 추가 (수정됨)
+make -j$(nproc --all) ARCH=arm64 LLVM=1 LLVM_IAS=1 O=$WORKDIR/out CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- modules
 
 send_msg "✅ Module build success! Collecting & uploading module files..."
 
